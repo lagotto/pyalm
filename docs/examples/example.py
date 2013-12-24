196c41cd030cb42bec6139670bc5647a2dd53b49
cameronneylon <cn@cameronneylon.net>
2013-12-24 14:57:38 +0000

    cleaning up after documentation re-org

--- docs/examples/example.py
+++ docs/examples/example.py
@@ -8,34 +8,38 @@ article = alm.get_alm("10.1371/journal.pone.0029797", info="summary")
 article
 
 ### @export "print-biblio"
-print article.title
-print article.url
+article.title
+article.url
 
 ### @export "print-ids"
-print article.doi
-print article.pmid
+article.doi
+article.pmid
 
 ### @export "print-stats"
-print article.views
-print article.citations
+article.views
+article.citations
 
 ### @export "multiple-dois"
-articles = alm.get_alm("10.1371/journal.pone.0029797,10.1371/journal.pone.0029798", info="summary")
-print "Number of articles retrieved:", len(articles), "\n"
+articles = alm.get_alm(
+               "10.1371/journal.pone.0029797,10.1371/journal.pone.0029798",
+               info="summary")
+len(articles)
 for article in articles:
-    print article.title, "DOI:", article.doi, "Views:", article.views
+    print "DOI:", article.doi,
+    print "Views:", article.views
 
 ### @export "get-event-level-cites"
-article = alm.get_alm("10.1371/journal.pone.0029797", info="event", source="crossref")
-print article.sources['crossref']
+article = alm.get_alm("10.1371/journal.pone.0029797",
+                      info="event", source="crossref")
+article.sources['crossref']
 
 ### @export "print-cites-metrics"
-print 'Total cites from Crossref:', article.sources['crossref'].metrics.total
-print '..is the same as # cites:', article.sources['crossref'].metrics.citations
-print '...and no shares:', article.sources['crossref'].metrics.shares
+article.sources['crossref'].metrics.total
+article.sources['crossref'].metrics.citations
+article.sources['crossref'].metrics.shares
 
 ### @export "print-cites-events"
-print 'The first recorded Crossref citation of the article:\n', article.sources['crossref'].events[0]
+article.sources['crossref'].events[0]
 
 
 

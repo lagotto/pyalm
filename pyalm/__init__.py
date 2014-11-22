420681242fa754d8d4365a7f25e5b4d30d216f66
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-22 09:13:22 -0800

    changed api key approach to follow that of the sunlightlab python client  #4

--- pyalm/__init__.py
+++ pyalm/__init__.py
@@ -17,3 +17,43 @@ from .config import *
 from .events import *
 from .pyalm import get_alm
 from .utils import *
+
+import os
+
+def _get_env_vars():
+	kk = [pyalm.config.PLOS_KEY_ENVVAR, pyalm.config.ELIFE_KEY_ENVVAR,
+		pyalm.config.CROSSREF_KEY_ENVVAR, pyalm.config.PKP_KEY_ENVVAR,
+		pyalm.config.COPERNICUS_KEY_ENVVAR, pyalm.config.PENSOFT_KEY_ENVVAR,
+		pyalm.config.PLOS_URL_ENVVAR, pyalm.config.ELIFE_URL_ENVVAR,
+		pyalm.config.CROSSREF_URL_ENVVAR, pyalm.config.PKP_URL_ENVVAR,
+		pyalm.config.COPERNICUS_URL_ENVVAR, pyalm.config.PENSOFT_URL_ENVVAR]
+	keydict = {}
+	for x in kk:
+		keydict[x] = os.environ.get(x)
+	return keydict
+
+def _attempt_key_load():
+    """
+    This function (which will be auto-called on import of :mod:`pyalm`),
+    will attempt to pull the API Keys from a few places.
+
+    .. note::
+        This function is implemented to let the enviroment variable override
+        the file read key. Keep this in mind when debugging silly issues.
+    """
+
+    try:
+		fp = os.path.expanduser(pyalm.config.PYALM_LOCATION)
+		fd = open(fp, 'r')
+		vals = fd.read().splitlines()
+		pyalm.config.APIS = dict(iter([i.split("=") for i in vals]))
+    except IOError as e:
+        if e.errno != 2:
+            warnings.warn('pyalm file %s exists but could not be opened: %s' % (
+                pyalm.config.PYALM_LOCATION, str(e)))
+    try:
+        pyalm.config.APIS = _get_env_vars()
+    except KeyError as e:
+        pass
+
+_attempt_key_load()

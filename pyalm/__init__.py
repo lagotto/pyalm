6cfc2cf723ba0c957d18db2ed2c6c9ec4d2179c0
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-24 09:09:06 -0800

    edited initialization script to output the same api key dict as before #4

--- pyalm/__init__.py
+++ pyalm/__init__.py
@@ -19,6 +19,16 @@ from .pyalm import get_alm
 from .utils import *
 
 import os
+import re
+
+def _insert_items(keydict, mdict, i):
+	for p in keydict.iterkeys():
+		if bool(re.search(i, p, re.IGNORECASE)):
+			if bool(re.search('URL', p)):
+				mdict[i]['url'] = keydict[p]
+			if bool(re.search('KEY', p)):
+				mdict[i]['key'] = keydict[p]
+	return mdict
 
 def _get_env_vars():
 	kk = [pyalm.config.PLOS_KEY_ENVVAR, pyalm.config.ELIFE_KEY_ENVVAR,
@@ -30,30 +40,43 @@ def _get_env_vars():
 	keydict = {}
 	for x in kk:
 		keydict[x] = os.environ.get(x)
-	return keydict
+
+	mdict = {}
+	mdict['plos'] = {}
+	mdict['elife'] = {}
+	mdict['pkp'] = {}
+	mdict['pensoft'] = {}
+	mdict['crossref'] = {}
+	mdict['copernicus'] = {}
+
+	names = ['plos','elife','pkp','pensoft','crossref','copernicus']
+	for x in names:
+		_insert_items(keydict, mdict, x)
+
+	return mdict
 
 def _attempt_key_load():
-    """
-    This function (which will be auto-called on import of :mod:`pyalm`),
-    will attempt to pull the API Keys from a few places.
+	"""
+	This function (which will be auto-called on import of :mod:`pyalm`),
+	will attempt to pull the API Keys from a few places.
 
-    .. note::
-        This function is implemented to let the enviroment variable override
-        the file read key. Keep this in mind when debugging silly issues.
-    """
+	.. note::
+		This function is implemented to let the enviroment variable override
+		the file read key. Keep this in mind when debugging silly issues.
+	"""
 
-    try:
+	try:
 		fp = os.path.expanduser(pyalm.config.PYALM_LOCATION)
 		fd = open(fp, 'r')
 		vals = fd.read().splitlines()
 		pyalm.config.APIS = dict(iter([i.split("=") for i in vals]))
-    except IOError as e:
-        if e.errno != 2:
-            warnings.warn('pyalm file %s exists but could not be opened: %s' % (
-                pyalm.config.PYALM_LOCATION, str(e)))
-    try:
-        pyalm.config.APIS = _get_env_vars()
-    except KeyError as e:
-        pass
+	except IOError as e:
+		if e.errno != 2:
+			warnings.warn('pyalm file %s exists but could not be opened: %s' % (
+				pyalm.config.PYALM_LOCATION, str(e)))
+	try:
+		pyalm.config.APIS = _get_env_vars()
+	except KeyError as e:
+		pass
 
 _attempt_key_load()
